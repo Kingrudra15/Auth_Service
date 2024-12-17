b7fcb9088f3a7e6292e9f7296d659c13b166fa28
Kingrudra15 <omabhi5@gmail.com>
2024-12-17 23:14:30 +0530

    added getuserby id in repo layer

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ const { PORT } = require('./config/serverConfig')
 
 const apiRoutes = require('./routes/index');
 
-
-
-
+//const UserRepository = require('./repository/user-repository')
 
 const app = express();
 const prepareAndStartServer = () => {
@@ -15,9 +13,12 @@ const prepareAndStartServer = () => {
     app.use(bodyParser.urlencoded({extended:true}))
     app.use('/api',apiRoutes)
 
-
-    app.listen(PORT , () => {
+    app.listen(PORT , async () => {
         console.log(`server started ON port: ${PORT}`);
+        // const repo = new UserRepository();
+        // const response = await repo.getById(1)
+        // console.log(response);
+        
         
     })
 }
