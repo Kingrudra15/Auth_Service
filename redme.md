a7e844e835fb251aa695b9d4df1e4a57ff5571e5
Kingrudra15 <omabhi5@gmail.com>
2025-01-06 23:17:36 +0530

    Added roles and user through user_role table

--- redme.md
+++ redme.md
@@ -1,2 +1,5 @@
 Auth service
  ```npx sequelize db:create command to create db```
+ try to implement verify email in auth sevice assignment
+
+ //DB_SYNC = true
--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ const { PORT } = require('./config/serverConfig')
 const apiRoutes = require('./routes/index');
 //const UserService = require('./services/user-service')
 //const UserRepository = require('./repository/user-repository')
-
+const db = require('./models/index')
+//const {User,Role} = require('./models/index')
 const app = express();
 const prepareAndStartServer = () => {
 
@@ -15,6 +16,18 @@ const prepareAndStartServer = () => {
 
     app.listen(PORT , async () => {
         console.log(`server started ON port: ${PORT}`);
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter:true});
+        }
+
+        // const u1 = await User.findByPk(3);        
+        // const r1 = await Role.findByPk(2)
+        
+        // //u1.addRole(r1);
+        // const response = await u1.hasRole(r1)
+        // console.log(response);
+        
+
         // const repo = new UserRepository();
         // const response = await repo.getById(1)
         // console.log(response);
